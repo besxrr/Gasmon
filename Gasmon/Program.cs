75f76566e0824658639c337bfbbb4278121ced0d
Besar <besar.krasniqi@softwire.com>
2021-11-24 15:06:23 +0000

    init

---
+++ Gasmon/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Gasmon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
