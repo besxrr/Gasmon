989823f91738d6147a2fa3254102c7cf4ac47bcb
Besar <besar.krasniqi@softwire.com>
2021-11-24 17:16:18 +0000

    Added in queue and delete queue

--- Gasmon/Program.cs
+++ Gasmon/Program.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Amazon;
 using Amazon.Runtime;
 using Amazon.S3;
 using Amazon.S3.Model;
+using Amazon.SimpleNotificationService;
+using Amazon.SQS;
+using Amazon.SQS.Model;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 
@@ -14,11 +18,17 @@ namespace Gasmon
 {
     class Program
     {
+        public static AWSCredentials Credentials = new BasicAWSCredentials(Environment.GetEnvironmentVariable("accessKey"),
+            Environment.GetEnvironmentVariable("secretKey"));
         static async Task Main(string[] args)
         {
-            var credentials = new BasicAWSCredentials(Environment.GetEnvironmentVariable("accessKey"),
-                Environment.GetEnvironmentVariable("secretKey"));
-            var s3Client = new AmazonS3Client(credentials, RegionEndpoint.EUWest1);
+            await CreateSqsQueue();
+            await GetS3Information();
+        }
+
+        private static async Task GetS3Information()
+        {
+            var s3Client = new AmazonS3Client(Credentials, RegionEndpoint.EUWest1);
             const string bucketName = "eventprocessing-swapprentices20-locationss3bucket-qu0txg2hhzj2";
 
             var objectResponse = await s3Client.GetObjectAsync(bucketName, "locations.json");
@@ -27,5 +37,23 @@ namespace Gasmon
             var jsonResponse = await objectReader.ReadToEndAsync();
             JsonConvert.DeserializeObject<List<Location>>(jsonResponse);
         }
+
+        private static async Task CreateSqsQueue()
+        {
+            var topicArn = "arn:aws:sns:eu-west-1:552908040772:EventProcessing-SWApprentices2021-snsTopicSensorDataPart1-DF8ZTFFN636Z";
+            var sqsClient = new AmazonSQSClient(Credentials, RegionEndpoint.EUWest1);
+            var snsClient = new AmazonSimpleNotificationServiceClient(Credentials, RegionEndpoint.EUWest1);
+            
+            var createQueueResponse = sqsClient.CreateQueueAsync(new CreateQueueRequest
+            {
+                QueueName = "beskra-locations-queue"
+            }).Result;
+            var queueUrl = createQueueResponse.QueueUrl;
+            await snsClient.SubscribeQueueAsync(topicArn, sqsClient, queueUrl);
+            Thread.Sleep(TimeSpan.FromSeconds(10));
+            await sqsClient.DeleteQueueAsync(queueUrl);
+        }
+        
+        
     }
 }
